db32ed731f147e38399e019b680bf8a664aecd8a
misi123412 <101482637+misi123412@users.noreply.github.com>
2022-11-19 01:28:42 +0100

    base html & css w/ animations
    none

---
+++ js/nav.animation.js
@@ -0,0 +1,10 @@
+
+
+
+function navbar_load() {
+    const nav_1 = document.getElementById('nav-1')
+    const nav_2 = document.getElementById('nav-2')
+    const nav_3 = document.getElementById('nav-3')
+    
+
+}
